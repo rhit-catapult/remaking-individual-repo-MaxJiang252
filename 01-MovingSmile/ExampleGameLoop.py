65d729311197514d6d8e43c1ae8467690a64c686
MaxJiang252 <maxjiang252@gmail.com>
2026-06-10 11:24:48 -0400

    1. Get to know pygames

--- 01-MovingSmile/ExampleGameLoop.py
+++ 01-MovingSmile/ExampleGameLoop.py
@@ -18,37 +18,25 @@ screen = pygame.display.set_mode((640, 480))
 
 # This is a loop that will run forever, simply because True is always true
 while True:
-    # Here's another loop inside of the first loop. Notice the indentation,
-    # moving one tab width into the while loop makes this statement part of the
-    # loop instead of outside of it.
     for event in pygame.event.get():
-        # Let's just print all the events that happen in our window, wonder
-        # what those could be?
-        print(event)
-
-        # we must allow our users to quit the game right? I mean not everyone
-        # wants to play forever.
-        # This is a conditional statement, i.e., the line sys.exit() will ONLY
-        # execute when event.type is equal to pygame.QUIT (this is what ==
-        # means). 
         if event.type == pygame.QUIT:
             sys.exit()
 
         # Additional interactions with events
 
     # TODO 01: Make the background white by uncommenting the line below
-    # screen.fill(pygame.Color("Gray"))
+    screen.fill(pygame.Color("Gray"))
 
     # Draw things on the screen
 
     # TODO 02: Try to draw a circle (any size, any color, anywhere)
-    # pygame.draw.circle(screen, color, pos, radius, width(optional)  )
+    pygame.draw.circle(screen, pygame.Color("White"), (screen.get_width() // 2  , 300), 50, 0)
 
     # TODO 03: Try to draw a red circle in the middle of the screen with a radius 100
-    # pygame.draw.circle(screen, color, pos, radius, width(optional)  )
+    pygame.draw.circle(screen, pygame.Color("Red"), (screen.get_width() // 2, screen.get_height() // 2), 100, 0)
 
     # TODO 04: Try to draw a yellow circle with the center exactly in the lower left corner of the screen, radius 10
-    # pygame.draw.circle(screen, color, pos, radius, width(optional)  )
+    pygame.draw.circle(screen, pygame.Color(255, 255, 0), (120, 470), 10, 0)
 
     # This will make sure that things appear on our screen, without this
     # update, everything we do will not be visible!
