d743af23da4833235d3e427ef7cbf99c707f1a9f
MaxJiang252 <maxjiang252@gmail.com>
2026-06-11 11:38:29 -0400

    4. Fourth day progress

--- 03-ClickInTheCircle/ClickInTheCircle.py
+++ 03-ClickInTheCircle/ClickInTheCircle.py
@@ -7,7 +7,7 @@ def distance(point1, point2):
     point2_x = point2[0]
     point1_y = point1[1]
     point2_y = point2[1]
-
+    return math.sqrt((point1_x - point2_x) ** 2 + (point1_y - point2_y) ** 2)
     # TODO 4: Return the actual distance between point 1 and point 2.
     #  Hint: you will need the math library for the sqrt function.
     #       distance = sqrt(   (delta x) ** 2 + (delta y) ** 2  )
@@ -19,9 +19,11 @@ def main():
     screen = pygame.display.set_mode((400, 400))
     pygame.display.set_caption("Mouse click positions")
     font = pygame.font.Font(None, 25)
+    message_text = ' '
 
     # TODO 8: Load the "drums.wav" file into the pygame music mixer
-
+    pygame.mixer.music.load("drums.wav")
+    
     instruction_text = 'Click in the circle'
     text_color = (222, 222, 0)
     instructions_image = font.render(instruction_text, True, text_color)
@@ -31,14 +33,25 @@ def main():
     circle_radius = 50
     circle_border_width = 3
 
-    message_text = ''
+    message_image = font.render(message_text, True, (122, 237, 201))
 
     while True:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click_position = event.pos
+                distance_from_circle = distance(click_position, circle_center)
+                if distance_from_circle <= circle_radius:
+                    print("Play the music")
+                    pygame.mixer.music.play(-1)  # Play the music in a loop
+                    screen.blit(message_image, (25, 350))
+                    message_text = 'Bullseye!'
+                else:
+                    print("Stop the music")
+                    pygame.mixer.music.stop()  # Stop the music
+                    message_text = 'You missed!'
             # TODO 2: For a MOUSEBUTTONDOWN event get the click position.
                 # TODO 3: Determine the distance between the click position and the circle_center using the distance
                 # TODO 3:   function and save the result into a variable called distance_from_circle
@@ -50,11 +63,13 @@ def main():
         screen.fill(pygame.Color("Black"))
 
         # TODO 1: Draw the circle using the screen, circle_color, circle_center, circle_radius, and circle_border_width
-
+        pygame.draw.circle(screen, circle_color, circle_center, circle_radius, circle_border_width)
         # TODO 6: Create a text image (render the text) based on the message_text with the color (122, 237, 201)
-
+        message_image = font.render(message_text, True, (122, 237, 201))
         screen.blit(instructions_image, (25, 25))
         # TODO 7: Draw (blit) the message to the user that says 'Bullseye!' or 'You missed!'
+        screen.blit(message_image, (25, 350))
+        
 
         pygame.display.update()
 
--- 04-Raindrops/RainyDay.py
+++ 04-Raindrops/RainyDay.py
@@ -1,11 +1,18 @@
-import pygame
+import os
+import random  # Note this!
 import sys
 import time  # Note this!
-import random  # Note this!
+
+import pygame
 
 
 class Raindrop:
     def __init__(self, screen, x, y):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.speed = random.randint(5, 15)
+
         """ Creates a Raindrop sprite that travels down at a random speed. """
         # TODO 8: Initialize this Raindrop, as follows:
         #     - Store the screen.
@@ -17,24 +24,31 @@ class Raindrop:
     def move(self):
         """ Move the self.y value of the Raindrop down the screen (y increase) at the self.speed. """
         # TODO 11: Change the  y  position of this Raindrop by its speed.
-        pass
+        self.y += self.speed
 
     def off_screen(self):
+    
         """ Returns true if the Raindrop y value is not shown on the screen, otherwise false. """
         # Note: this will be used for testing, but not used in the final version of the code for the sake of simplicity.
-        # TODO 13: Return  True  if the  y  position of this Raindrop is greater than 800.
-        pass
+        # TODO 13: Return  True  if the  y  position of this Raindrop is greater than the screen height.
+        return self.y > self.screen.get_height()
 
     def draw(self):
         """ Draws this sprite onto the screen. """
         # TODO 9: Draw a vertical line that is 5 pixels long, 2 pixels thick,
         #      from the current position of this Raindrop (use either a black or blue color).
-        pass
+        pygame.draw.line(self.screen, pygame.Color("blue"), (self.x, self.y), (self.x, self.y + 5), 2)
 
 
 class Hero:
     def __init__(self, screen, x, y, with_umbrella_filename, without_umbrella_filename):
         """ Creates a Hero sprite (Mike) that does not move. If hit by rain he'll put up his umbrella. """
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.image_umbrella = pygame.image.load(with_umbrella_filename)
+        self.image_no_umbrella = pygame.image.load(without_umbrella_filename)  
+        self.last_hit_time = 0
         # TODO 16: Initialize this Hero, as follows:
         #     - Store the screen.
         #     - Set the initial position of this Hero to x and y.
@@ -43,7 +57,6 @@ class Hero:
         #     - Set the "last hit time" to 0.
         #   Use instance variables:
         #      screen  x  y  image_umbrella   image_no_umbrella  last_hit_time.
-        pass
 
     def draw(self):
         """ Draws this sprite onto the screen. """
@@ -52,16 +65,25 @@ class Hero:
         #     If the current time is greater than this Hero's last_hit_time + 1,
         #       draw this Hero WITHOUT an umbrella,
         #       otherwise draw this Hero WITH an umbrella.
-        pass
+        if time.time() > self.last_hit_time + 1:
+            self.screen.blit(self.image_no_umbrella, (self.x, self.y))
+        else:
+            self.screen.blit(self.image_umbrella, (self.x, self.y))
 
     def hit_by(self, raindrop):
         """ Returns true if the given raindrop is hitting this Hero, otherwise false. """
         # TODO 19: Return True if this Hero is currently colliding with the given Raindrop.
-        pass
+        my_hit_box = pygame.Rect(self.x, self.y, self.image_no_umbrella.get_width(), self.image_no_umbrella.get_height())
+        return my_hit_box.collidepoint(raindrop.x, raindrop.y + 5)
 
 
 class Cloud:
     def __init__(self, screen, x, y, image_filename):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load(image_filename)
+        self.raindrops = []
         """ Creates a Cloud sprite that will produce Raindrop objects.  The cloud will be moving around. """
         # TODO 24: Initialize this Cloud, as follows:
         #     - Store the screen.
@@ -70,7 +92,7 @@ class Cloud:
         #     - Create a list for Raindrop objects as an empty list called raindrops.
         #   Use instance variables:
         #      screen  x  y  image   raindrops.
-        pass
+        
 
     def draw(self):
         """ Draws this sprite onto the screen. """
@@ -87,15 +109,41 @@ class Cloud:
 
 
 def main():
+    print("main is called")
+    asset_folder = os.path.dirname(os.path.abspath(__file__))
     """ Main game loop that creates the sprite objects, controls interactions, and draw the screen. """
     # TODO 1: Initialize the game, display a caption, and set   screen   to a 1000x600 Screen.
-
+    pygame.init()
+    screen = pygame.display.set_mode((1000, 600))
+    pygame.display.set_caption("Rainy Days")
     # TODO 2: Make a Clock
+    clock = pygame.time.Clock()
+    test_drop = Raindrop(screen, 310, 10)
+    mike = Hero(
+        screen,
+        200,
+        400,
+        os.path.join(asset_folder, "Mike_umbrella.png"),
+        os.path.join(asset_folder, "Mike.png"),
+    )
+    alyssa = Hero(
+        screen,
+        700,
+        400,
+        os.path.join(asset_folder, "Alyssa_umbrella.png"),
+        os.path.join(asset_folder, "Alyssa.png"),
+    )
+    cloud = Cloud(screen, 300, 50, os.path.join(asset_folder, "cloud.png"))
     # TODO 7: As a temporary test, make a new Raindrop called test_drop at x=320 y=10
     # TODO 15: Make a Hero, named mike, with appropriate images, starting at position x=200 y=400.
     # TODO 15: Make a Hero, named alyssa, with appropriate images, starting at position x=700 y=400.
     # TODO 23: Make a Cloud, named cloud, with appropriate images, starting at position x=300 y=50.
-
+    while True:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            
     # TODO 3: Enter the game loop, with a clock tick of 60 (or so) at each iteration.
         # TODO 4:   Make the pygame.QUIT event stop the game.
 
@@ -109,13 +157,27 @@ def main():
         #          If you want something to continually happen while holding the key, put it after the events loop.
 
         # TODO 5: Inside the game loop, draw the screen (fill with white)
-
+        screen.fill(pygame.Color("white"))
+        
         # --- begin area of test_drop code that will be removed later
         # TODO 12: As a temporary test, move test_drop
+        test_drop.move()
+        if test_drop.off_screen():
+            test_drop.y = 10
+        test_drop.draw()
         # TODO 14: As a temporary test, check if test_drop is off screen, if so reset the y position to 10
         # TODO 10: As a temporary test, draw test_drop
-
+        test_drop.draw()
         # TODO 20: As a temporary test, check if test_drop is hitting Mike (or Alyssa), if so set their last_hit_time
+        if mike.hit_by(test_drop):
+            mike.last_hit_time = time.time()
+            test_drop.x = 720
+            test_drop.y = 0
+            
+        if alyssa.hit_by(test_drop):
+            alyssa.last_hit_time = time.time()
+            test_drop.x = 310
+            test_drop.y = 0
         # TODO 22: Remove the code that reset the y of the test_drop when off_screen()
         #          Instead reset the test_drop y to 10 when mike is hit, additionally set the x to 750
         #          Then add similar code to alyssa that sets her last_hit_time and moves the test_drop to 10 320
@@ -132,9 +194,11 @@ def main():
             # Optional  - if the Raindrop is off the screen or hitting a Hero, remove it from the Cloud's list of raindrops.
 
         # TODO 18: Draw the Heroes (Mike and Alyssa)
-
+        mike.draw()
+        alyssa.draw()
         # TODO 6: Update the display and remove the pass statement below
-    pass
+        pygame.display.update()
 
 
 # TODO 0: Call main.
+main()
